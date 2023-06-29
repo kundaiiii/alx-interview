1c027b771108eb203139c4b3c7353faae07c060e
kundaiiii <kundaimm@yahoo.com>
2023-06-29 17:34:56 +0000

    mandatory task

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
       List of lists of integers representing the Pascal’s triangle
     '''
     lists = []
-    if n == 0:
+    if n <= 0:
         return lists
     for i in range(n):
         lists.append([])
